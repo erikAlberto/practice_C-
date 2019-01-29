1c733725b463014e82547c16e5f50f548e305cf0
Alberto Vargas Estrada <Alberto Vargas@jalasoft.local>
2019-01-28 20:20:10 -0400

    practice2

---
+++ Todo/Todo/UITestTodoApp/Pages/TaskFormPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UITestTodoApp.Controls;
+using Xamarin.UITest;
+namespace UITestTodoApp.Pages
+{
+    class TaskFormPage
+    {
+        IApp app;
+
+        TaskFormControl taskFormControl;
+
+        public TaskFormPage()
+        {
+            taskFormControl = new TaskFormControl();
+            app = AppInitializer.GetCurrentApp();
+        }
+
+        public void EnterTaskName(string TaskName)
+        {
+            var taskNameInputLambda = this.taskFormControl.TaskNameInput;
+            this.app.ClearText(taskNameInputLambda);
+            this.app.EnterText(taskNameInputLambda, TaskName);
+        }
+
+        public void EnterNotesTask(string TaskNote)
+        {
+            var taskNotesInputLambda = this.taskFormControl.TaskNoteInput;
+            this.app.ClearText(taskNotesInputLambda);
+            this.app.EnterText(taskNotesInputLambda, TaskNote);
+        }
+
+        public void SetTaskAsDone()
+        {
+            var taskDoneLambda = this.taskFormControl.DoneTaskSwitch;
+            this.app.DismissKeyboard();
+            this.app.TouchAndHold(taskDoneLambda);
+        }
+
+        public void SaveTask()
+        {
+            var saveTaskLambda = this.taskFormControl.SaveTaskButton;
+            this.app.DismissKeyboard();
+            this.app.Tap(saveTaskLambda);
+        }
+
+        public void DeleteTask()
+        {
+            var deleteTaskLambda = this.taskFormControl.DeleteTaskButton;
+            this.app.Tap(deleteTaskLambda);
+        }
+    }
+}
